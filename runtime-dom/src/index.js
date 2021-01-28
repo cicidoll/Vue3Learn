661a75ff6d7106c1273a72e61b83e3349a6a3272
cicidoll <ayaseemt@qq.com>
2021-01-28 20:59:52 +0800

    update runtime-dom\src\index.js

--- runtime-dom/src/index.js
+++ runtime-dom/src/index.js
@@ -25,11 +25,13 @@ export const Vue = {
   }
 
 /**
- * 粗糙的方法：容器
- * @param {Object} containerOrSelector 
+ * 粗糙的方法：返回容器变量
+ * @param {Object} containerOrSelector 容器元素或选择器
+ * @returns {HTMLElement} 返回一个DOM节点
  */
 const normalizeContainer = function (containerOrSelector) {
-  if (isHTMLElement(containerOrSelector)) return containerOrSelector
+  if (isHTMLElement(containerOrSelector)) 
+    return containerOrSelector
   const container = document.querySelector(containerOrSelector)
   return container
 }
@@ -37,20 +39,23 @@ const normalizeContainer = function (containerOrSelector) {
 /**
  * 判断对象是否为DOM节点
  * @param {Object} object 需要判断的对象
+ * @returns {Function} 返回一个函数，函数功能为判断对象是否为DOM节点
  */
-const isHTMLElement = function (object) {
-  return ( typeof HTMLElement === 'object' ) ?
-    function(object){
+const isHTMLElement = ( typeof HTMLElement === 'object' ) ?
+    function (object) {
       return object instanceof HTMLElement
     } :
-    function(object){
+    function (object) {
       return object && typeof object === 'objectect'
         && object.nodeType === 1
         && typeof object.nodeName === 'string'
     }
-}
 
-// 判断是否为函数
+/**
+ * 判断是否为函数
+ * @param {function} FunName 需要判断的函数变量名
+ * @returns {Boolean} 函数是否为参数
+ */
 const isFunction = function (FunName) {
   return typeof FunName === "function"
 }
